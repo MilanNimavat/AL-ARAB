dd8da1ddaa8719305ae76d64cd51e55e3be56718
Milan Nimavat <milanimavat171098@gmail.com>
2019-12-22 22:31:12 +0530

    needed stuff only

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post('/form',(req,res)=>{
     console.log(req.body.email1);
                   
     const msg = {
-        to: 'alarabworld19@gmail.com',					//receiver's email
+        to: 'alarabworld19@gmail.com',	//receiver's email
         from: req.body.email1,			//sender's email
         subject: req.body.subject1,				//Subject
         text: req.body.message1       ,		//content
